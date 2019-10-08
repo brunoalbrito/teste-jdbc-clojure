a458ada5c09f000cdcfd8079d5b5e344e5cab65e
brunoalbrito <bruno.brito@startmeup.com.br>
2019-10-08 07:39:53 -0300

    Adicionando docker-compose e fazendo criacao de tabela com datafuncionar

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.jdbc "0.7.0-alpha1"]
+                 [org.postgresql/postgresql "9.4.1212"]]
   :plugins [[org.clojars.benfb/lein-gorilla "0.6.0"]]
   :main ^:skip-aot teste-jdbc-clojure.core
   :target-path "target/%s"
--- src/teste_jdbc_clojure/core.clj
+++ src/teste_jdbc_clojure/core.clj
@@ -1,5 +1,21 @@
 (ns teste-jdbc-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.java.jdbc :as j])
   (:gen-class))
+
+(def db {:dbtype "postgresql"
+         :dbname "teste_jdbc"
+         :host "localhost"
+         :user "postgres"
+         :password ""})
+         
+(def state-sql (j/create-table-ddl :state [[:state_id :serial "PRIMARY KEY"]
+                                           [:state "VARCHAR(32)"]
+                                           [:abrv "VARCHAR(2)"]
+                                           [:time_test :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]]))
+
+
+; (j/execute! db [state-sql])
 
 (defn -main
   "I don't do a whole lot ... yet."
